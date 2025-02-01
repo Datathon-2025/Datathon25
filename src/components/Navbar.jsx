1cdf8e4780c37a56c44f8f29430d16132a6efe46
Unchanted <omkkdwivedi@gmail.com>
2025-02-01 16:01:03 +0530

    icons changed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,29 +1,33 @@
 import { Link } from "react-router-dom";
-import { LayoutDashboard, SquarePen, House, Users, LogOut } from "lucide-react";
+import { MdSpaceDashboard } from "react-icons/md";
+import { FaDrawPolygon } from "react-icons/fa6";
+import { FaHome } from "react-icons/fa";
+import { MdCampaign } from "react-icons/md";
+import { FiLogOut } from "react-icons/fi";
 
 export default function Navbar() {
   return (
     <nav className="flex justify-between bg-gray-800 p-4">
       <div className="flex gap-16 space-x-4">
         <Link to="/" className="text-white flex items-center space-x-2">
-          <House size="24" color="white" />
+          <FaHome size="24" color="white" />
           <span>Home</span>
         </Link>
         <Link to="/canvas" className="text-white flex items-center space-x-2">
-          <SquarePen size="24" color="white" />
+          <FaDrawPolygon size="24" color="white" />
           <span>Canvas</span>
         </Link>
         <Link to="/dashboard" className="text-white flex items-center space-x-2">
-          <LayoutDashboard size="24" color="white" />
+          <MdSpaceDashboard size="24" color="white" />
           <span>Dashboard</span>
         </Link>
         <Link to="/campaign" className="text-white flex items-center space-x-2">
-          <Users size="24" color="white" />
+          <MdCampaign size="24" color="white" />
           <span>Campaign</span>
         </Link>
       </div>
       <Link to="/logout" className="text-white flex items-center space-x-2">
-        <LogOut size="24" color="white" />
+        <FiLogOut size="26" color="white" />
         <span>Logout</span>
       </Link>
     </nav>
