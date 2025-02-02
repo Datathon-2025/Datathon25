eff6b4ad1259f3b838be3f4597da9ed4397e927c
Arnab-Afk <arnabbhowmik019@gmail.com>
2025-02-02 11:32:58 +0530

    stats card

--- src/components/StatsCard.jsx
+++ src/components/StatsCard.jsx
@@ -1,27 +1,47 @@
-const stats = [
-    { name: 'Number of deploys', value: '405' },
-    { name: 'Average deploy time', value: '3.65', unit: 'mins' },
-    { name: 'Number of servers', value: '3' },
-    { name: 'Success rate', value: '98.5%' },
+import { useState, useEffect } from "react"
+
+export default function StatsCard() {
+  const [clicks, setClicks] = useState(0)
+  const [impressions, setImpressions] = useState(0)
+  const [cpc, setCpc] = useState(728)
+  const [cpa, setCpa] = useState(0)
+
+  useEffect(() => {
+    fetch('http://13.61.152.203:5000/get_all_stats')
+      .then((response) => response.text())
+      .then((data) => {
+        const jsonData = JSON.parse(data)
+        console.log(jsonData)
+        setClicks(jsonData.number_of_clicks)
+        setImpressions(jsonData.number_of_impressions)
+        setCpc(jsonData.average_cpc)
+        setCpa(jsonData.cost)
+        console.log(jsonData.number_of_clicks)
+        console.log(jsonData.number_of_impressions)
+        console.log(jsonData.average_cpc)
+        console.log(jsonData.cost)
+      })
+  }, [])
+
+  const stats = [
+    { name: 'Number of clicks', value: clicks },
+    { name: 'Average CPC', value: cpc },
+    { name: 'Cost', value: cpa },
+    { name: 'Impressions', value: impressions },
   ]
-  
-  export default function StatsCard() {
-    return (
-      <div className="bg-gray-900 rounded-lg shadow-md">
-        <div className="mx-auto max-w-7xl">
-          <div className="grid grid-cols-1 gap-px bg-white/5 sm:grid-cols-2 lg:grid-cols-4">
-            {stats.map((stat) => (
-              <div key={stat.name} className="bg-gray-900 px-4 py-6 sm:px-6 lg:px-8">
-                <p className="text-sm font-medium leading-6 text-gray-400">{stat.name}</p>
-                <p className="mt-2 flex items-baseline gap-x-2">
-                  <span className="text-4xl font-semibold tracking-tight text-white">{stat.value}</span>
-                  {stat.unit ? <span className="text-sm text-gray-400">{stat.unit}</span> : null}
-                </p>
-              </div>
-            ))}
-          </div>
+
+  return (
+    <div className="bg-gray-900 rounded-lg shadow-md">
+      <div className="mx-auto max-w-7xl">
+        <div className="grid grid-cols-1 gap-px bg-white/5 sm:grid-cols-2 lg:grid-cols-4">
+          {stats.map((stat) => (
+            <div key={stat.name} className="p-4">
+              <p className="text-sm font-medium text-gray-400">{stat.name}</p>
+              <p className="mt-1 text-3xl font-semibold text-white">{stat.value}</p>
+            </div>
+          ))}
         </div>
       </div>
-    )
-  }
-  
+    </div>
+  )
+}
